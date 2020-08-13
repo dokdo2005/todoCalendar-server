a85b2d2d4774e81567da650fd33134b358c2632a
dokdo2005 <dokdo2005@gmail.com>
2020-08-13 12:39:24 +0900

    add todos

---
+++ controllers/todos.js
@@ -0,0 +1,17 @@
+const model = require("../models");
+const Todos = model.todo;
+
+module.exports = {
+    get: (req, res) => {
+
+    },
+    post: (req, res) => {
+
+    },
+    update: (req, res) => {
+
+    },
+    delete: (req, res) => {
+
+    }
+}
